8de4f0a54b5f8a71f4d3b766944e3654ed4b2ba0
Pavlomat <pavlov18matthew@gmail.com>
2022-11-18 11:19:03 +0300

    commit

--- ModernAutolayoutChapter14/MainViewController.swift
+++ ModernAutolayoutChapter14/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  ModernAutolayoutChapter14
 //
 //  Created by Pavlov Matvey on 14.11.2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
     }
-
-
+    
 }
-
